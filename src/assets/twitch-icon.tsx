2fec041d0bb5b17775f4de4a67f4b350b68e1611
goktugs <gokojapan5@gmail.com>
2023-08-24 15:11:33 +0300

    fix: header

--- src/assets/twitch-icon.tsx
+++ src/assets/twitch-icon.tsx
@@ -1,17 +1,26 @@
-import React from 'react'
+import React from "react";
 
 export const TwitchIcon = (props: React.SVGAttributes<SVGElement>) => {
-    return (
-        <svg  {...props} width="100%" height="100%" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <g clipPath="url(#clip0_1_428)">
-                <path d="M1.39282 0L0.34938 2.7825V13.9119H4.17376V16H6.26157L8.34751 13.9119H11.4769L15.6506 9.73969V0H1.39282ZM2.78313 1.39031H14.26V9.04281L11.825 11.4778H8.00001L5.91469 13.5631V11.4778H2.78313V1.39031ZM6.60876 8.34813H8.00001V4.17469H6.60876V8.34813ZM10.4341 8.34813H11.825V4.17469H10.4341V8.34813Z" fill="white" />
-            </g>
-            <defs>
-                <clipPath id="clip0_1_428">
-                    <rect width="16" height="16" fill="white" />
-                </clipPath>
-            </defs>
-        </svg>
-
-    )
-}
+  return (
+    <svg
+      {...props}
+      width="100%"
+      height="100%"
+      viewBox="0 0 16 16"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g clipPath="url(#clip0_1_428)">
+        <path
+          d="M1.39282 0L0.34938 2.7825V13.9119H4.17376V16H6.26157L8.34751 13.9119H11.4769L15.6506 9.73969V0H1.39282ZM2.78313 1.39031H14.26V9.04281L11.825 11.4778H8.00001L5.91469 13.5631V11.4778H2.78313V1.39031ZM6.60876 8.34813H8.00001V4.17469H6.60876V8.34813ZM10.4341 8.34813H11.825V4.17469H10.4341V8.34813Z"
+          fill="white"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_1_428">
+          <rect width="16" height="16" fill="white" />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ export default {
       colors: {
         "main-purple": "#2A2642",
         "main-purple-dark": "#231D3A",
+        "main-purple-light": "#453F62",
         "main-text-smoke": "#8F8FA7",
+      },
+      boxShadow: {
+        "main-input": "0px 4px 4px 0px rgba(0, 0, 0, 0.25)",
       },
     },
   },
